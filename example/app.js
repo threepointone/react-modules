1ae2f2609d2ea280ac38d40dad3cf0f5d303c39d
Sunil Pai <threepointone@gmail.com>
2016-09-22 02:05:17 +0530

    shuffle

--- example/app.js
+++ example/app.js
@@ -2,30 +2,31 @@ import React from 'react'
 import { Modules } from '../src' 
 import { BrowserRouter, Match, Miss, Link } from 'react-router'
 
-function log(msg = this) {
-  console.log(msg)
-  return this
-}
-
 export class App extends React.Component {
   render() {
     return <BrowserRouter>
       <div>
+
+        <ul>
+          <li><Link to="/">home</Link></li>
+          <li><Link to="/a">to a</Link></li>
+          <li><Link to="/b">to b</Link></li>          
+          <li><Link to="/asd">404</Link></li>
+        </ul>
+        
         <Match pattern="/" exactly render={() => <div>we home</div>}/>
         
         <Match pattern="/a" render={() => <Modules load={require('./a').default}>{
             A => A ? <A/> : <span>loading A...</span>
           }</Modules>}/>
+        
         <Match pattern="/b" render={() => <Modules load={require('./b').default}>{
             B => B ? <B/> : <span>loading B...</span>
           }</Modules>}/>
+        
         <Miss render={() => <span>no match</span>}/>
 
-        <ul>
-          <li><Link to="/a">to a</Link></li>
-          <li><Link to="/b">to b</Link></li>
-          <li><Link to="/">home</Link></li>
-        </ul>
+        
       </div>
     </BrowserRouter>
     
