360f95117d99a870dae0a35074c72ae628c5d080
Sunil Pai <threepointone@gmail.com>
2016-09-22 02:20:41 +0530

    .

--- example/index.js
+++ example/index.js
@@ -1,26 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './app'
-// import { hydrate } from '../src'
-
-// hydrate()
 
 render(<App/>, document.getElementById('app'))
-
-
-// let x = 
-
-//   <div>
-//     <Sidebar/>
-//     <Match pattern="/search/:term">{ ({ params }) => 
-//       <Modules entry="search" load={require('./search.js')}>{ Search => 
-//         <Search term={params.term} />
-//       }</Modules>
-//     }</Match>
-//     <Match pattern="/user/:uid">{ ({ params }) => 
-//       <Modules entry="user" load={require('./user.js')}>{ User => 
-//         <User uid={params.uid} />
-//       }</Modules>
-//     }</Match>  
-//     <Miss component={NotFound}/>
-//   </div>
